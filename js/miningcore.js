02250cb0f5c46760a8190aafe7c878953dabd9fa
HavekHD <126997530+HavekHD@users.noreply.github.com>
2023-03-21 21:02:37 -0500

    Update miningcore.js

--- js/miningcore.js
+++ js/miningcore.js
@@ -23,14 +23,14 @@
 
 
 // read WebURL from current browser
-var WebURL         = "http://havekhd.com";  // Website URL is:  https://domain.com/
+var WebURL         = "https://havekhd.com";  // Website URL is:  https://domain.com/
 // WebURL correction if not ends with /
 if (WebURL.substring(WebURL.length-1) != "/")
 {
 	WebURL = WebURL + "/";
 	console.log('Corrected WebURL, does not end with / -> New WebURL : ', WebURL);
 }
-var API            = "http://api.havekhd.com/api/";   						// API address is:  https://domain.com/api/
+var API            = "https://api.havekhd.com/api/";   						// API address is:  https://domain.com/api/
 // API correction if not ends with /
 if (API.substring(API.length-1) != "/")
 {
